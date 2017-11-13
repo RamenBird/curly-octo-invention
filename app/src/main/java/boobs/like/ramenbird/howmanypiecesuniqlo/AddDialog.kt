bee83483120cebaf248e66d2e5b317c4eaf8a7de
firebird <liuyuhao1211@gmail.com>
2017-11-13 23:23:54 +0800

    1234

--- app/src/main/java/boobs/like/ramenbird/howmanypiecesuniqlo/AddDialog.kt
+++ app/src/main/java/boobs/like/ramenbird/howmanypiecesuniqlo/AddDialog.kt
@@ -6,11 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import boobs.like.ramenbirds.howmanypiecesuniqlo.R
+import kotlinx.android.synthetic.main.add_dialog.*
 
 /**
  * Created by The great and powerful Android 18 on 2017/11/13.
  */
 class AddDialog : DialogFragment() {
+    private lateinit var tags : List<String>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        tags = arguments?.getStringArrayList("tags") ?: emptyList()
+    }
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View?
-            = inflater?.inflate(R.layout.add_dialog, container)
+            = inflater?.inflate(R.layout.add_dialog, null)
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        dialog.setTitle("Input product name")
+        input.setOnClickListener { it -> parse(name_input.text.toString()) }
+    }
+
+    fun parse(s: String) {
+
+    }
 }
